31ab4f0154ff744b9abf10ccd1d6dbe0cd60eea6
Christopher Ryan Halbersma <chalbersma@auditboard.com>
2025-08-14 13:14:50 -0700

    Multi Page Try 1

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # sphinx-to-coda-action
 
---------
-*This Project is being migrated over to auditboard/shpinx-to-coda-action Please use that repository*
---------
-
 An action to look at an intersphinx item, generate a webpage of it and upload that thing to a particular page in Coda.io
--- src/parse_and_upload.py
+++ src/parse_and_upload.py
@@ -20,6 +20,8 @@ import jinja2
 
 import bs4
 
+DYNAMIC_LIMIT = 100
+
 
 def get_argparse():
     """
@@ -32,7 +34,8 @@ def get_argparse():
                         default=os.environ.get("SPHINX_BASE_URI"))
     parser.add_argument("-f", "--objectfile", help="Intersphinx File", required=False,
                         default=os.environ.get("OBJECTS_FILE", "build/html/objects.inv"))
-    parser.add_argument("-c", "--linkclass", help="For HTML Files, what class are internal links saved as.", required=False,
+    parser.add_argument("-c", "--linkclass", help="For HTML Files, what class are internal links saved as.",
+                        required=False,
                         default=os.environ.get("LINK_CLASS", "reference internal"))
     parser.add_argument("-i", "--docID", help="CodaIO Document (Category) ID", required=False,
                         default=os.environ.get("DOCID"))
@@ -69,13 +72,57 @@ if __name__ == "__main__":
     logger = logging.getLogger("parse_and_upload.py")
     wanted_format = "html"
 
-    if os.path.isfile(args.objectfile) is False:
-        raise FileNotFoundError("No Inventory file found at {}.".format(args.objectfile))
-    elif os.path.isfile(args.template) is False:
-        raise FileNotFoundError("No Template file found at {}.".format(args.template))
+    all_files = list()
+    dynamic_pageId = False
 
-    else:
-        intersphinx_file = pathlib.Path(args.objectfile)
+    if os.path.isfile(args.objectfile) is False and os.path.isdir(args.objectfile) is False:
+        raise FileNotFoundError("No Inventory file or directory found at {}.".format(args.objectfile))
+    elif os.path.isfile(args.objectfile):
+        all_files = [pathlib.Path(args.objectfile)]
+
+    elif os.path.isdir(args.objectfile):
+
+        dynamic_pageId = True
+
+        for root, _, files in os.walk(args.objectfile):
+            for file_name in files:
+                this_full_path = os.path.join(root, file_name)
+                if re.match("\.html$", file_name, re.IGNORECASE):
+                    all_files.append(this_full_path)
+                else:
+                    logger.info("Ignoreing File {this_full_path} not in".format(this_full_path=this_full_path))
+
+        # Collect all Pages in DocID
+        all_docs_uri = urllib.parse.urlparse("https://coda.io/apis/v1/docs/{doc_id}/pages".format(doc_id=args.docID))
+
+        get_more = True
+        extra_params = dict()
+        all_pages = dict()
+
+        while get_more:
+            all_pages_response = requests.get(all_docs_uri.geturl(),
+                                              params={"limit": DYNAMIC_LIMIT, **extra_params},
+                                              headers={"Authorization": "Bearer " + args.token})
+
+            all_pages_response.raise_for_status()
+
+            results = all_pages_response.json()
+
+            # Handle Multiple
+            if len(results["items"]) == 100:
+                get_more = True
+                extra_params["pageToken"] = results["nextPageToken"]
+            else:
+                get_more = False
+
+            for this_page_details in results["items"]:
+                all_pages[this_page_details["subtitle"]] = {"og_data": this_page_details,
+                                                            "found_match": False,
+                                                            "alt_parent": None}
+
+    for this_filename in all_files:
+
+        intersphinx_file = pathlib.Path(this_filename)
         response_object = {"update_time": time.ctime()}
         project_name = "Unspecified"
 
@@ -88,10 +135,10 @@ if __name__ == "__main__":
                 template_string = template_fobj.read()
 
                 html_template = jinja2.Environment(loader=jinja2.BaseLoader,
-                                                  autoescape=jinja2.select_autoescape(
-                                                      enabled_extensions=('html', 'xml'),
-                                                      default_for_string=False,
-                                                  )).from_string(template_string)
+                                                   autoescape=jinja2.select_autoescape(
+                                                       enabled_extensions=('html', 'xml'),
+                                                       default_for_string=False,
+                                                   )).from_string(template_string)
 
                 rendered_html = html_template.render({"inventory": intersphinx_inventory,
                                                       "baseuri": args.uribase})
@@ -132,17 +179,80 @@ if __name__ == "__main__":
                 for admonition_div in source_html_obj.find_all('div', {'class': "admonition"}):
                     admonition_div.wrap(source_html_obj.new_tag("aside"))
 
-                spacey_rendered_html = str(source_html_obj) #.replace("\n", "")
+                spacey_rendered_html = str(source_html_obj)  # .replace("\n", "")
                 rendered_html = re.sub(r"\n+", "\n", spacey_rendered_html)
 
-                print(rendered_html)
+                # print(rendered_html)
 
         ## Coda Stuff
 
-        pages_uri = urllib.parse.urlparse("https://coda.io/apis/v1/docs/{doc_id}/pages/{page_id}".format(doc_id=args.docID,
-                                                                                                           page_id=args.pageID))
+        if dynamic_pageId is False:
+            # There's a Single, Specified Page
 
-        #print(pages_uri.geturl())
+            pages_uri = urllib.parse.urlparse(
+                "https://coda.io/apis/v1/docs/{doc_id}/pages/{page_id}".format(doc_id=args.docID,
+                                                                               page_id=args.pageID))
+
+            update_payload = {
+                "name": project_name,
+                "subtitle": "Generated Time: {ctime}".format(ctime=response_object["update_time"]),
+                "contentUpdate": {
+                    "insertionMode": "replace",
+                    "canvasContent": {
+                        "format": "html",
+                        "content": rendered_html
+                    }
+                }
+            }
+
+        else:
+
+            if this_filename in all_pages.keys():
+
+                pages_uri = "https://coda.io/apis/v1/docs/{doc_id}/pages/{page_id}".format(doc_id=args.docID,
+                                                                                           page_id=
+                                                                                           all_pages[this_filename][
+                                                                                               "og_data"]["id"])
+
+                all_pages[this_filename]["found_match"] = True
+
+
+            else:
+                # Dynamic Page Generation
+                # Create a New Page
+
+                new_page = "https://coda.io/apis/v1/docs/{doc_id}/pages".format(doc_id=args.docID)
+
+                post_obj = {
+                    "name": project_name,
+                    "subtitle": this_filename,
+                    "pageContent": {
+                        "format": "html",
+                        "content": rendered_html
+                    }
+                }
+
+                new_page_response = requests.post(new_page, json=post_obj)
+                new_page_response.raise_for_status()
+
+                new_page_data = new_page_response.json()
+
+                pages_uri = urllib.parse.urlparse(
+                    "https://coda.io/apis/v1/docs/{doc_id}/pages/{page_id}".format(doc_id=args.docID,
+                                                                                   page_id=new_page_data["id"])
+                )
+
+            update_payload = {
+                "name": project_name,
+                "subtitle": this_filename,
+                "contentUpdate": {
+                    "insertionMode": "replace",
+                    "canvasContent": {
+                        "format": "html",
+                        "content": rendered_html
+                    }
+                }
+            }
 
         try:
             page_response = requests.get(pages_uri.geturl(),
@@ -167,14 +277,13 @@ if __name__ == "__main__":
                 }
             }
 
-
             try:
                 pu_response = requests.put(pages_uri.geturl(),
-                                             headers={"Authorization": "Bearer " + args.token,
-                                                      "Content-Type": "application/json"
-                                                      },
-                                             json=update_payload
-                                             )
+                                           headers={"Authorization": "Bearer " + args.token,
+                                                    "Content-Type": "application/json"
+                                                    },
+                                           json=update_payload
+                                           )
                 pu_response.raise_for_status()
             except Exception as pu_error:
                 logger.error("Unable to Update the Page.")
@@ -183,6 +292,10 @@ if __name__ == "__main__":
             else:
                 # I've updated the page
                 response_object = {**response_object, **pu_response.json()}
+                print(json.dumps(response_object, indent=4))
 
-    print(json.dumps(response_object, indent=4))
+    if dynamic_pageId is True:
+        logger.info("Future Clean up Unmatched Documents")
+        pass
+
     sys.exit(0)
